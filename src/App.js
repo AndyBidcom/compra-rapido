a4841f3aa3f0594e034dd0c38479c1cca5a953f6
Andres Barreto <andres@bidcom.com.ar>
2020-11-28 00:17:58 -0300

    update proyecto compraland

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Proyecto Ecommerce Compra Rapido
+          Proyecto Ecommerce Compraland
         </p>
       </header>
     </div>
