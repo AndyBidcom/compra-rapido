b28a847bc9a40aaa3f9a3c67513088215408a289
Andres Barreto <andres@bidcom.com.ar>
2020-12-20 11:06:38 -0300

    CartWidget modal c/state 2
    Añado funcionalidad a la X de close

--- src/components/General/CartWidget.jsx
+++ src/components/General/CartWidget.jsx
@@ -1,6 +1,5 @@
 import {useState} from 'react';
 import cart from '../../images/cart.svg';
-import CartWidgetModal from '../General/CartWidgetModal.jsx';
 
 const CartWidget = ({quantity}) => {
     const [showCartWidgetModal,setShowCartWidgetModal] = useState(false);
@@ -13,7 +12,14 @@ const CartWidget = ({quantity}) => {
                 <img src={cart} alt="Cart" />
                 <span class="badge badge-secondary quantity">{quantity}</span>
             </a>
-            <CartWidgetModal show={showCartWidgetModal} />
+            <div className={`cart-widget ${showCartWidgetModal ? 'active' : ''}`}>
+                <a href="#" class="close" onClick={openCartWidgetModal}>x</a>
+                <h4>Tu carrito</h4>
+                <div class="row item-cart-widget">
+                    <div class="col-9 text-left">Título producto</div>
+                    <div class="col-3 text-right">$1200</div>
+                </div>
+            </div>            
         </>
     )
 }
