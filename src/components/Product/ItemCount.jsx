78b5befd54f48dc8b1e76fbfeaa232e75cdfff19
Andres Barreto <andres@bidcom.com.ar>
2020-12-20 22:29:57 -0300

    Desafio: Contador con botón
    Añado además funcionalidad para modificar manualmente el número validando que sea numérico siempre y >= 1.
    Además deshabilito el botón de " - " (menos) cuado la cantidad es 1.

---
+++ src/components/Product/ItemCount.jsx
@@ -0,0 +1,36 @@
+import {useState} from 'react';
+
+const ItemCount = () => {
+    const [Quantity,setQuantity] = useState(1);
+    const AddQuantity = () => {
+        setQuantity(parseInt(Quantity) + 1);
+    }
+    const RemoveQuantity = () => {
+        if(Quantity>1){
+            setQuantity(parseInt(Quantity) - 1);
+        }        
+    }    
+    const ManualQuantity = (e) => {
+        if (e.target.validity.valid){
+            if(e.target.value == '' || e.target.value < 1){
+                setQuantity(1);
+            }else{
+                setQuantity(e.target.value);
+            }            
+        }
+        //setQuantity(e.target.value);
+    }
+    return (
+        <>
+            <div className="item-count">
+                <div className="row no-gutters">
+                    <button name="less" className={`btn-count btn-less${Quantity} col-3`} onClick={RemoveQuantity}>-</button>
+                    <input type="tel" className="input-count col-6" pattern="^-?[0-9]\d*\.?\d*$" value={Quantity} onChange={ManualQuantity} />
+                    <button name="more" className="btn-count col-3" onClick={AddQuantity}>+</button>
+                </div>
+            </div>
+            <a href="#" className="btn btn-outline-primary btn-sm d-block">Agregar a Carrito</a>
+        </>
+    )
+}
+export default ItemCount;
--- src/components/Product/ItemListProduct.jsx
+++ src/components/Product/ItemListProduct.jsx
@@ -1,12 +1,14 @@
+import ItemCount from './ItemCount.jsx';
+
 const ItemListProduct = ({title,price,link,photo,discount}) =>(
     <div className="item-list col-sm-3">
-        <img src={photo} alt={title} />
-        <h3>{title}</h3>
+        <a href={link}><img src={photo} alt={title} />
+        <h3>{title}</h3></a>
         <div class="row no-gutters">
             <div class="col-5 price-item align-self-center">${price}</div>
             <div class="col-7 text-right align-self-center"><span className="badge badge-success">{discount}</span></div>
         </div>
-        <a href={link} className="btn btn-outline-primary btn-sm d-block">Agregar a Carrito</a>
+        <ItemCount/>        
     </div>
 )
 export default ItemListProduct;
